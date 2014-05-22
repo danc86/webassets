5f32cabed38d31738b7731949077595afbaee94a
Michael Elsdörfer <michael@elsdoerfer.com>
2014-05-22 06:04:15 +0200

    Update some test filter outputs.
    (cherry picked from commit 1abd59b284ceb2bfc1b83768a19b9e8e94d6a268)
    
    Conflicts:
    	tests/test_filters.py

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -611,7 +611,7 @@ class TestCoffeeScript(TempEnvironmentHelper):
         self.env.config['COFFEE_NO_BARE'] = True
         self.create_files({'in': "@a = 1"})
         self.mkbundle('in', filters='coffeescript', output='out.js').build()
-        assert self.get('out.js') == '(function() {\n\n  this.a = 1;\n\n}).call(this);\n'
+        assert self.get('out.js') == '(function() {\n  this.a = 1;\n\n}).call(this);\n'
 
         self.env.config['COFFEE_NO_BARE'] = False
         self.create_files({'in': "@a = 1"})
@@ -1202,4 +1202,4 @@ class TestTypeScript(TempEnvironmentHelper):
 
     def test(self):
         self.mkbundle('foo.ts', filters='typescript', output='out.js').build()
-        assert self.get("out.js") == """var X = (function () {\n    function X() { }\n    return X;\n})();\n"""
+        assert self.get("out.js") == 'var X = (function () {\n    function X() {\n    }\n    return X;\n})();\n'
